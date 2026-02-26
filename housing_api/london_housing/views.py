5ef3a78db72d162b01d15e09d79e913bc7e1d535
unknown <sc23s2e@leeds.ac.uk>
2026-02-26 15:05:40 +0000

    Can create new housing/areas via api request and also filter/search existing housing

--- housing_api/london_housing/views.py
+++ housing_api/london_housing/views.py
@@ -1,3 +1,105 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Housing, Area
 
 # Create your views here.
+
+#allows testing POST
+@csrf_exempt
+def housing_list(request):
+    #if GET need to read all of the housing listing
+    if request.method == 'GET':
+        houses = Housing.objects.all()
+
+        #potential filters for the housing
+        area_filter = request.GET.get('area')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        min_beds = request.GET.get('bedrooms')
+        prop_type = request.GET.get('type')
+
+        #apply filters to the query results
+        #filter case insensitive 
+        if area_filter:
+            houses = houses.filter(area__name__icontains=area_filter)
+
+        #greater or equal to
+        if min_price:
+            houses = houses.filter(price__gte=min_price)
+
+        #less than or equal to
+        if max_price:
+            houses = houses.filter(price__lte=max_price)
+
+        if min_beds:
+            houses = houses.filter(bedrooms__gte=min_beds)
+
+        if prop_type:
+            houses = houses.filter(property_type__icontains=prop_type)
+
+        #map database to a dictionary
+        data = []
+
+        #loop through each house + add to dictionary
+        for house in houses:
+            data.append({
+                "id": house.housing_id,
+                "address": house.address,
+                "property_type": house.property_type,
+                "price": float(house.price),
+                "bedrooms": house.bedrooms,
+                "area_name": house.area.name
+            })
+
+        if houses.count() == 0:
+            #return HttpResponse(status=204) can use this if correct code wanted but doesnt display anything
+            return JsonResponse({"Output": "No houses match", "houses": []}, status=200)
+
+        #return as a Json Response
+        return JsonResponse({"number of houses": houses.count(), "houses": data}, safe=False, status=200)
+
+    #post for creating a new house
+    elif request.method == 'POST':
+        #parse incoming JSON and check for existing area (may have to create)
+        try: 
+            body = json.loads(request.body)
+            area_name = body.get('area_name', '').strip()
+            address = body.get('address', '').strip()
+
+            #validate if containing an address/area name
+            #400 code for an invalid request 
+            if not address or not area_name:
+                return JsonResponse({
+                    "error": "Both 'address' and 'area_name' are required fields and cannot be empty."
+                }, status=400)
+            
+            area_obj, created = Area.objects.get_or_create(name=area_name)
+
+            #create new house record
+            new_house = Housing.objects.create(
+                area=area_obj,
+                address=address,
+                property_type=body.get('property_type'),
+                price=body.get('price'),
+                area_sqft=body.get('area_sqft'),
+                bedrooms=body.get('bedrooms', 0),
+                bathrooms=body.get('bathrooms', 0),
+                receptions=body.get('receptions', 0),
+                for_sale=body.get('for_sale', False),
+                for_rent=body.get('for_rent', False)
+            )
+
+            return JsonResponse({"message": "House Created", "id":new_house.housing_id}, status=201)
+        
+        #see if Json was coded incorrectly
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON format provided."}, status=400)
+        
+        #outputs the exception if the creation fails
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+
+
+
